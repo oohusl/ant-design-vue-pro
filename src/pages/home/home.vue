e755773910695fe082c43ec0ebb6ac8f3c716ec6
254651308 <254651308@qq.com>
2020-11-14 15:16:24 +0800

    ui

---
+++ src/pages/home/home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="page-header-content">
+    <div class="content">
+      <div class="content-title">
+        欢迎使用xx管理系统
+      </div>
+      <!-- <div>市场销售 | 房XX - 张某某</div> -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  methods: {
+  }
+}
+</script>
