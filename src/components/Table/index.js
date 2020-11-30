24ca95afe94afc8f4f867aedfc5260b48603bdc4
oohusl <oohusl@163.com>
2020-11-30 12:35:57 +0800

    page

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -154,34 +154,35 @@ export default {
       )
       parameter.page = parameter.page - 1
       const result = this.data(parameter)
+      const that = this
       // 对接自己的通用数据接口需要修改下方代码中的 r.pageNo, r.totalCount, r.data
       // eslint-disable-next-line
       if ((typeof result === 'object' || typeof result === 'function') && typeof result.then === 'function') {
         result.then(r => {
-          this.localPagination = this.showPagination && Object.assign({}, this.localPagination, {
+          that.localPagination = that.showPagination && Object.assign({}, that.localPagination, {
+            current: (pagination && pagination.current) || that.showPagination && that.localPagination.current || that.pageNum,
             total: r['x-total-count'], // 返回结果中的总记录数
-            showSizeChanger: this.showSizeChanger,
             pageSize: (pagination && pagination.pageSize) ||
-              this.localPagination.pageSize
+              that.localPagination.pageSize
           }) || false
           // 为防止删除数据后导致页面当前页面数据长度为 0 ,自动翻页到上一页
-          if (r.length === 0 && this.showPagination && this.localPagination.current > 1) {
-            this.localPagination.current--
-            this.loadData()
+          if (r.length === 0 && that.showPagination && that.localPagination.current > 1) {
+            that.localPagination.current--
+            that.loadData()
             return
           }
 
           // 这里用于判断接口是否有返回 r.totalCount 且 this.showPagination = true 且 pageNo 和 pageSize 存在 且 totalCount 小于等于 pageNo * pageSize 的大小
           // 当情况满足时，表示数据不满足分页大小，关闭 table 分页功能
           try {
-            if ((['auto', true].includes(this.showPagination) && r.totalCount <= (r.pageNo * this.localPagination.pageSize))) {
-              this.localPagination.hideOnSinglePage = true
+            if ((['auto', true].includes(that.showPagination) && r.totalCount <= (r.pageNo * that.localPagination.pageSize))) {
+              that.localPagination.hideOnSinglePage = true
             }
           } catch (e) {
-            this.localPagination = false
+            that.localPagination = false
           }
-          this.localDataSource = r // 返回结果中的数组数据
-          this.localLoading = false
+          that.localDataSource = r // 返回结果中的数组数据
+          that.localLoading = false
         })
       }
     },
