bacc9c1b86db7a3547167a5ea0b4e7b6bcdc6a5e
254651308 <254651308@qq.com>
2021-05-10 14:16:19 +0800

    Update HouseTypeEdit.vue

--- src/pages/workspace/HouseTypeEdit.vue
+++ src/pages/workspace/HouseTypeEdit.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <a-form :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }" :labelAlign="'right'">
+    <a-form :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }" :labelAlign="'right'" layout="inline">
       <a-form-item label="房屋结构">
         <a-input style="width:100px" addon-after="室" v-model="houseTypeEdit.room"></a-input
         ><a-input style="width:100px" addon-after="厅" v-model="houseTypeEdit.hall"></a-input
@@ -8,31 +8,32 @@
         ><a-input style="width:100px" addon-after="卫" v-model="houseTypeEdit.toilet"></a-input>
       </a-form-item>
       <a-form-item label="建筑面积">
-        <a-input addon-after="m²" v-model="houseTypeEdit.acreage"></a-input>
+        <a-input style="width:200px" addon-after="m²" v-model="houseTypeEdit.acreage"></a-input>
       </a-form-item>
       <a-form-item label="参考单价">
-        <a-input addon-after="元" v-model="houseTypeEdit.referenceUnitPrice"></a-input>
+        <a-input style="width:200px" addon-after="元" v-model="houseTypeEdit.referenceUnitPrice"></a-input>
       </a-form-item>
       <a-form-item label="参考总价">
-        <a-input addon-after="万元" v-model="houseTypeEdit.referenceTotalPrice"></a-input>
+        <a-input style="width:200px" addon-after="万元" v-model="houseTypeEdit.referenceTotalPrice"></a-input>
       </a-form-item>
       <a-form-item label="房屋类型">
-        <a-input v-model="houseTypeEdit.typesOfHouse"></a-input>
+        <a-input style="width:200px" v-model="houseTypeEdit.typesOfHouse"></a-input>
       </a-form-item>
       <a-form-item label="房屋朝向">
-        <a-input v-model="houseTypeEdit.towards"></a-input>
+        <a-input style="width:200px" v-model="houseTypeEdit.towards"></a-input>
       </a-form-item>
       <a-form-item label="房屋层高">
-        <a-input addon-after="米" v-model="houseTypeEdit.floorHeight"></a-input>
+        <a-input style="width:200px" addon-after="米" v-model="houseTypeEdit.floorHeight"></a-input>
       </a-form-item>
-      <!-- <a-form-item label="梯户比">
-        <a-input addon-after="梯" style="width:100px" v-model="houseTypeEdit.analysis"></a-input><a-input addon-after="户" style="width:100px" v-model="houseTypeEdit.analysis"></a-input>
-      </a-form-item> -->
+      <a-form-item label="梯户比">
+        <a-input addon-after="梯" style="width:100px" v-model="houseTypeEdit.ladderNumber"></a-input
+        ><a-input addon-after="户" style="width:100px" v-model="houseTypeEdit.doorNumber"></a-input>
+      </a-form-item>
       <a-form-item label="户型存量">
-        <a-input addon-after="套" v-model="houseTypeEdit.unitInventory"></a-input>
+        <a-input style="width:200px" addon-after="套" v-model="houseTypeEdit.unitInventory"></a-input>
       </a-form-item>
       <a-form-item label="户型分析">
-        <a-input v-model="houseTypeEdit.analysis"></a-input>
+        <a-input style="width:200px" v-model="houseTypeEdit.analysis"></a-input>
       </a-form-item>
       <a-form-item label=" " :colon="false">
         <a-upload
@@ -75,6 +76,11 @@ export default {
   computed () {
     this.refresh()
   },
+  created () {
+    const t = this.dealLadderRadio(this.houseTypeEdit.ladderRatio)
+    this.houseTypeEdit.ladderNumber = t[0]
+    this.houseTypeEdit.doorNumber = t[1]
+  },
   data () {
     return {
       imageEditVisible: false,
@@ -85,13 +91,17 @@ export default {
       toDelete: [],
       previewVisible: false,
       previewImage: '',
-      houseTypeFiles: this.houseAnalysis.photoUrl ? [{
-            uid: this.houseAnalysis.id,
-            imageId: this.houseAnalysis.id,
-            status: 'done',
-            name: '户型图',
-            url: this.houseAnalysis.photoUrl
-          }] : [],
+      houseTypeFiles: this.houseAnalysis.photoUrl
+        ? [
+            {
+              uid: this.houseAnalysis.id,
+              imageId: this.houseAnalysis.id,
+              status: 'done',
+              name: '户型图',
+              url: this.houseAnalysis.photoUrl
+            }
+          ]
+        : [],
       houseTypes: [],
       houseTypeEdit: this.houseAnalysis,
       houseTypeOptions: [
@@ -118,9 +128,12 @@ export default {
     saveHouseType () {
       this.houseTypeEdit.unitTypeName = `${this.houseTypeEdit.room}室${this.houseTypeEdit.hall || 0}厅${!this
         .houseTypeEdit.kitchen || 0}厨${this.houseTypeEdit.toilet || 0}卫`
-        if (this.houseTypeFiles.length <= 0) {
-          this.houseTypeEdit.photoUrl = ''
-        }
+      if (this.houseTypeEdit.ladderNumber && this.houseTypeEdit.doorNumber) {
+        this.houseTypeEdit.ladderRatio = `${this.houseTypeEdit.ladderNumber}梯${this.houseTypeEdit.doorNumber}户`
+      }
+      if (this.houseTypeFiles.length <= 0) {
+        this.houseTypeEdit.photoUrl = ''
+      }
       return saveAnalysis(this.houseTypeEdit)
         .then(e => {
           if (this.houseTypeFiles.length > 0 && this.houseTypeFiles[0].file) {
@@ -145,13 +158,17 @@ export default {
     },
     refresh () {
       this.houseTypeEdit = this.houseAnalysis
-      this.houseTypeFiles = this.houseAnalysis.photoUrl ? [{
-            uid: this.houseAnalysis.id,
-            imageId: this.houseAnalysis.id,
-            status: 'done',
-            name: '户型图',
-            url: this.houseAnalysis.photoUrl
-          }] : []
+      this.houseTypeFiles = this.houseAnalysis.photoUrl
+        ? [
+            {
+              uid: this.houseAnalysis.id,
+              imageId: this.houseAnalysis.id,
+              status: 'done',
+              name: '户型图',
+              url: this.houseAnalysis.photoUrl
+            }
+          ]
+        : []
     },
     beforeHouseTypeUpload (file) {
       getBase64(file).then(url => {
@@ -180,6 +197,10 @@ export default {
         default:
           break
       }
+    },
+    dealLadderRadio (ladderRadio) {
+      const t = ((ladderRadio || '') + '梯户').split('梯')
+      return [t[0], t[1].split('户')[0]]
     }
   }
 }
