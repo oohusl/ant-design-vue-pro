977049bee88932c2d5038ff15264720bc299fe93
254651308 <254651308@qq.com>
2021-05-23 11:29:52 +0800

    Update HouseTypeEdit.vue

--- src/pages/workspace/HouseTypeEdit.vue
+++ src/pages/workspace/HouseTypeEdit.vue
@@ -1,57 +1,88 @@
 <template>
   <div>
     <a-form :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }" :labelAlign="'right'">
-      <a-form-item label="房屋结构">
+      <a-form-item label="房屋结构" :labelCol="{ span: 3 }" :wrapperCol="{ span: 21 }">
         <a-input style="width:100px" addon-after="室" v-model="houseTypeEdit.room"></a-input
         ><a-input style="width:100px" addon-after="厅" v-model="houseTypeEdit.hall"></a-input
         ><a-input style="width:100px" addon-after="厨" v-model="houseTypeEdit.kitchen"></a-input
         ><a-input style="width:100px" addon-after="卫" v-model="houseTypeEdit.toilet"></a-input>
       </a-form-item>
-      <a-form-item label="建筑面积">
-        <a-input addon-after="m²" v-model="houseTypeEdit.acreage"></a-input>
-      </a-form-item>
-      <a-form-item label="参考单价">
-        <a-input addon-after="元" v-model="houseTypeEdit.referenceUnitPrice"></a-input>
-      </a-form-item>
-      <a-form-item label="参考总价">
-        <a-input addon-after="万元" v-model="houseTypeEdit.referenceTotalPrice"></a-input>
-      </a-form-item>
-      <a-form-item label="房屋类型">
-        <a-input v-model="houseTypeEdit.typesOfHouse"></a-input>
-      </a-form-item>
-      <a-form-item label="房屋朝向">
-        <a-input v-model="houseTypeEdit.towards"></a-input>
-      </a-form-item>
-      <a-form-item label="房屋层高">
-        <a-input addon-after="米" v-model="houseTypeEdit.floorHeight"></a-input>
-      </a-form-item>
-      <a-form-item label="梯户比">
-        <a-input addon-after="梯" style="width:100px" v-model="houseTypeEdit.ladderNumber"></a-input
-        ><a-input addon-after="户" style="width:100px" v-model="houseTypeEdit.doorNumber"></a-input>
-      </a-form-item>
-      <a-form-item label="户型存量">
-        <a-input addon-after="套" v-model="houseTypeEdit.unitInventory"></a-input>
-      </a-form-item>
-      <a-form-item label="户型分析">
-        <a-input v-model="houseTypeEdit.analysis"></a-input>
-      </a-form-item>
-      <a-form-item label=" " :colon="false">
-        <a-upload
-          :file-list="houseTypeFiles"
-          accept="image/*"
-          list-type="picture-card"
-          :before-upload="beforeHouseTypeUpload"
-          :remove="handleHouseTypeRemove"
-          @preview="handlePreview"
-        >
-          <div>
-            <a-icon type="plus" />
-            <div class="ant-upload-text">
-              上传图片
+      <a-row :gutter="24">
+        <a-col :span="12">
+          <a-form-item label="建筑面积">
+            <a-input addon-after="m²" v-model="houseTypeEdit.acreage"></a-input>
+          </a-form-item>
+        </a-col>
+        <a-col :span="12">
+          <a-form-item label="建筑面积">
+            <a-input addon-after="m²" v-model="houseTypeEdit.acreage"></a-input>
+          </a-form-item>
+        </a-col>
+      </a-row>
+      <a-row :gutter="24">
+        <a-col :span="12">
+          <a-form-item label="参考总价">
+            <a-input addon-after="万元" v-model="houseTypeEdit.referenceTotalPrice"></a-input>
+          </a-form-item>
+        </a-col>
+        <a-col :span="12">
+          <a-form-item label="房屋类型">
+            <a-input v-model="houseTypeEdit.typesOfHouse"></a-input>
+          </a-form-item>
+        </a-col>
+      </a-row>
+      <a-row :gutter="24">
+        <a-col :span="12">
+          <a-form-item label="房屋朝向">
+            <a-input v-model="houseTypeEdit.towards"></a-input>
+          </a-form-item>
+        </a-col>
+        <a-col :span="12">
+          <a-form-item label="房屋层高">
+            <a-input addon-after="米" v-model="houseTypeEdit.floorHeight"></a-input>
+          </a-form-item>
+        </a-col>
+      </a-row>
+      <a-row :gutter="24">
+        <a-col :span="12">
+          <a-form-item label="梯户比">
+            <a-input addon-after="梯" style="width:98px" v-model="houseTypeEdit.ladderNumber"></a-input
+            ><a-input addon-after="户" style="width:98px" v-model="houseTypeEdit.doorNumber"></a-input>
+          </a-form-item>
+        </a-col>
+        <a-col :span="12">
+          <a-form-item label="户型存量">
+            <a-input addon-after="套" v-model="houseTypeEdit.unitInventory"></a-input>
+          </a-form-item>
+        </a-col>
+      </a-row>
+      <a-row :gutter="24">
+        <a-col
+          :span="24"
+        ><a-form-item label="户型分析" :labelCol="{ span: 3 }" :wrapperCol="{ span: 21 }">
+          <a-input v-model="houseTypeEdit.analysis"></a-input> </a-form-item
+        ></a-col>
+      </a-row>
+      <a-row :gutter="24">
+        <a-col :span="3"> </a-col>
+        <a-col :span="21">
+          <a-upload
+            :file-list="houseTypeFiles"
+            accept="image/*"
+            list-type="picture-card"
+            :before-upload="beforeHouseTypeUpload"
+            :remove="handleHouseTypeRemove"
+            @preview="handlePreview"
+          >
+            <div>
+              <a-icon type="plus" />
+              <div class="ant-upload-text">
+                上传图片
+              </div>
             </div>
-          </div>
-        </a-upload>
-      </a-form-item>
+          </a-upload>
+        </a-col>
+      </a-row>
     </a-form>
   </div>
 </template>
@@ -72,9 +103,6 @@ export default {
       }
     },
     toCreate: { type: Boolean, default: false }
-  },
-  computed () {
-    this.refresh()
   },
   created () {
     const t = this.dealLadderRadio(this.houseTypeEdit.ladderRatio)
