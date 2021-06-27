1c34c2d2f5213d1de81a8975f17efe26b97304c9
qichenchu <aswezxqcc@LIVE.CN>
2021-06-27 23:56:45 +0800

    打印测试

--- src/pages/workspace/BuildingOverview.vue
+++ src/pages/workspace/BuildingOverview.vue
@@ -1,78 +1,96 @@
 <template>
-  <div class="building-overview">
-    <a-layout :style="{ background: 'transparent', padding: '0', height: '100%', padding: '0 24px' }">
-      <a-layout-header :style="{ background: 'transparent', padding: '0', height: '174px', color: 'white' }">
-        <a-layout :style="{ background: 'transparent', padding: '0', height: '174px' }">
-          <a-layout-sider :style="{ background: 'transparent', padding: '0 10px', height: '174px' }" width=300>
-            <a-layout :style="{ background: 'transparent', padding: '0', height: '174px' }">
-              <a-layout-header :style="{ background: 'transparent', padding: '0', height: '48px' }">
-                <div class="community-header">
-                  <div class="community-name">{{ houseSelect.communityName }}</div>
-                  <div class="community-price">
-                    <span>5月份参考均价</span>
-                    <span style="font-size:18px;font-weight: bold;text-align: center;">￥{{ houseSelect.averageLlistedPrice == null ? '--' : houseSelect.averageLlistedPrice.toLocaleString() }}元/m²</span>
+  <div>
+    <div>
+      <button @click="printpdf">print</button>
+    </div>
+    <div class="building-overview" ref="print">
+      <a-layout :style="{ background: 'transparent', padding: '0', height: '100%', padding: '0 24px' }">
+        <a-layout-header :style="{ background: 'transparent', padding: '0', height: '174px', color: 'white' }">
+          <a-layout :style="{ background: 'transparent', padding: '0', height: '174px' }">
+            <a-layout-sider :style="{ background: 'transparent', padding: '0 10px', height: '174px' }" width=300>
+              <a-layout :style="{ background: 'transparent', padding: '0', height: '174px' }">
+                <a-layout-header :style="{ background: 'transparent', padding: '0', height: '48px' }">
+                  <div class="community-header">
+                    <div class="community-name">{{ houseSelect.communityName }}</div>
+                    <div class="community-price">
+                      <span>5月份参考均价</span>
+                      <span style="font-size:18px;font-weight: bold;text-align: center;">￥{{ houseSelect.averageLlistedPrice == null ? '--' : houseSelect.averageLlistedPrice.toLocaleString() }}元/m²</span>
+                    </div>
                   </div>
-                </div>
-              </a-layout-header>
-              <a-layout-content class="building-params">
-                <a-descriptions title="" :column="4">
-                  <a-descriptions-item label="楼栋" :span="2">
-                    {{ houseSelect.buildingNumber ? houseSelect.buildingNumber + '栋' : '' }}
-                  </a-descriptions-item>
-                  <a-descriptions-item label="绿化" :span="2">
-                    {{ houseSelect.greeningRate ? houseSelect.greeningRate + '%' : '' }}
-                  </a-descriptions-item>
-                  <a-descriptions-item label="年代" :span="2">
-                    {{ houseSelect.constructionAge ? houseSelect.constructionAge + '年' : '' }}
-                  </a-descriptions-item>
-                  <a-descriptions-item label="停车" :span="2">
-                    {{ houseSelect.parkingSpacesNumber }}
-                  </a-descriptions-item>
-                  <a-descriptions-item label="户数" :span="2">
-                    {{ houseSelect.householdsNumber ? houseSelect.householdsNumber + '户' : '' }}
-                  </a-descriptions-item>
-                  <a-descriptions-item label="物业费" :span="2">
-                    {{ houseSelect.propertyCosts ? houseSelect.propertyCosts + '元/m²' : '' }}
-                  </a-descriptions-item>
-                  <a-descriptions-item label="物业" :span="4">
-                    {{ houseSelect.propertyCompany }}
-                  </a-descriptions-item>
-                  <a-descriptions-item label="开发" :span="4">
-                    {{ houseSelect.developer }}
-                  </a-descriptions-item>
-                </a-descriptions>
-              </a-layout-content>
-            </a-layout>
-          </a-layout-sider>
-          <a-layout-content :style="{ background: 'transparent', padding: '0', height: '174px' }">
-            <a-layout :style="{ background: 'transparent', padding: '0 5px 0 34px', height: '174px', lineHeight: '1.4' }">
-              <a-layout :style="{ background: 'transparent', padding: '0' }">
-                <a-layout-sider :style="{ background: 'transparent', padding: '0'}" width=345>
-                  <div class="price7label">近7日挂牌行情</div>
-                  <div class="price7">87864元/㎡</div>
-                  <div class="price30label">近30日同比</div>
-                  <div class="price30">
-                    <span>挂牌价持平</span>
-                    <span>挂牌量持平</span>
-                  </div>
-                </a-layout-sider>
-                <a-layout-content :style="{ background: 'transparent', padding: '0', position: 'relative'}">
-                  <div style="position: absolute;bottom: 0px;font-size: 18px;line-height: 44px;margin-left: 92px;">未来60天成交价走势<span style="margin-left: 20px;display: inline-block;width: 89px;color: rgb(0,37,200);font-weight: bold;text-align: center;letter-spacing: 16px;">上涨</span></div>
+                </a-layout-header>
+                <a-layout-content class="building-params">
+                  <a-descriptions title="" :column="4">
+                    <a-descriptions-item label="楼栋" :span="2">
+                      {{ houseSelect.buildingNumber ? houseSelect.buildingNumber + '栋' : '' }}
+                    </a-descriptions-item>
+                    <a-descriptions-item label="绿化" :span="2">
+                      {{ houseSelect.greeningRate ? houseSelect.greeningRate + '%' : '' }}
+                    </a-descriptions-item>
+                    <a-descriptions-item label="年代" :span="2">
+                      {{ houseSelect.constructionAge ? houseSelect.constructionAge + '年' : '' }}
+                    </a-descriptions-item>
+                    <a-descriptions-item label="停车" :span="2">
+                      {{ houseSelect.parkingSpacesNumber }}
+                    </a-descriptions-item>
+                    <a-descriptions-item label="户数" :span="2">
+                      {{ houseSelect.householdsNumber ? houseSelect.householdsNumber + '户' : '' }}
+                    </a-descriptions-item>
+                    <a-descriptions-item label="物业费" :span="2">
+                      {{ houseSelect.propertyCosts ? houseSelect.propertyCosts + '元/m²' : '' }}
+                    </a-descriptions-item>
+                    <a-descriptions-item label="物业" :span="4">
+                      {{ houseSelect.propertyCompany }}
+                    </a-descriptions-item>
+                    <a-descriptions-item label="开发" :span="4">
+                      {{ houseSelect.developer }}
+                    </a-descriptions-item>
+                  </a-descriptions>
                 </a-layout-content>
               </a-layout>
-              <a-layout-footer :style="{ background: 'transparent', padding: '0', display: 'flex', height: '45px',lineHeight: '34px',paddingRight: '20px' }">
-                <div class="sale-label">目前在售同户型房源 <span>1套</span></div>
-                <div class="sent-buiding">参考租金</div>
-                <div class="sent-label">房型<span>两室一厅</span></div>
-                <div class="sent-label">价格<span>￥1000</span></div>
-              </a-layout-footer>
-            </a-layout>
-          </a-layout-content>
-        </a-layout>
-      </a-layout-header>
-      <a-layout-content>
-      </a-layout-content>
-    </a-layout>
+            </a-layout-sider>
+            <a-layout-content :style="{ background: 'transparent', padding: '0', height: '174px' }">
+              <a-layout :style="{ background: 'transparent', padding: '0 5px 0 34px', height: '174px', lineHeight: '1.4' }">
+                <a-layout :style="{ background: 'transparent', padding: '0' }">
+                  <a-layout-sider :style="{ background: 'transparent', padding: '0'}" width=345>
+                    <div class="price7label">近7日挂牌行情</div>
+                    <div class="price7">87864元/㎡</div>
+                    <div class="price30label">近30日同比</div>
+                    <div class="price30">
+                      <span>挂牌价持平</span>
+                      <span>挂牌量持平</span>
+                    </div>
+                  </a-layout-sider>
+                  <a-layout-content :style="{ background: 'transparent', padding: '0', position: 'relative'}">
+                    <div style="position: absolute;bottom: 0px;font-size: 18px;line-height: 44px;margin-left: 92px;">未来60天成交价走势<span style="margin-left: 20px;display: inline-block;width: 89px;color: rgb(0,37,200);font-weight: bold;text-align: center;letter-spacing: 16px;">上涨</span></div>
+                  </a-layout-content>
+                </a-layout>
+                <a-layout-footer :style="{ background: 'transparent', padding: '0', display: 'flex', height: '45px',lineHeight: '34px',paddingRight: '20px' }">
+                  <div class="sale-label">目前在售同户型房源 <span>1套</span></div>
+                  <div class="sent-buiding">参考租金</div>
+                  <div class="sent-label">房型<span>两室一厅</span></div>
+                  <div class="sent-label">价格<span>￥1000</span></div>
+                </a-layout-footer>
+              </a-layout>
+            </a-layout-content>
+          </a-layout>
+        </a-layout-header>
+        <a-layout-content :style="{ background: 'transparent', padding: '0'}">
+          <div style="display:flex;height:100%">
+            <div style="flex:1">
+              <a-layout :style="{ background: 'transparent', padding: '0'}">
+                <a-layout-header :style="{ background: 'transparent', padding: '0'}">
+
+                </a-layout-header>
+                <a-layout-content :style="{ background: 'transparent', padding: '0'}">
+
+                </a-layout-content>
+              </a-layout>
+            </div>
+            <div style="flex:1"></div>
+          </div>
+        </a-layout-content>
+      </a-layout>
+    </div>
   </div>
 </template>
 
@@ -99,8 +117,10 @@ export default {
     queryHouse () {
       getHouseDetail(this.houseSelect.id).then(d => {
         this.houseSelect = d
-        console.log(d)
       })
+    },
+    printpdf () {
+      this.$print(this.$refs.print)
     }
   }
 }
@@ -109,7 +129,7 @@ export default {
 .building-overview {
   padding:0;
   background: url(~@/assets/print-bg.png);
-  height: 794px;
+  height: 792px;
   width: 1123px;;
 }
 .community-header {
