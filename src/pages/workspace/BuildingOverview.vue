0268d488aa5ef66f355dfd3756711434fb9896c2
qichenchu <aswezxqcc@LIVE.CN>
2021-07-04 19:57:18 +0800

    update print header

--- src/pages/workspace/BuildingOverview.vue
+++ src/pages/workspace/BuildingOverview.vue
@@ -4,18 +4,14 @@
       <button @click="printpdf">print</button>
     </div>
     <div class="building-overview" ref="print">
-      <a-layout :style="{ background: 'transparent', padding: '0', height: '100%', padding: '0 24px' }">
-        <a-layout-header :style="{ background: 'transparent', padding: '0', height: '174px', color: 'white' }">
-          <a-layout :style="{ background: 'transparent', padding: '0', height: '174px' }">
-            <a-layout-sider :style="{ background: 'transparent', padding: '0 10px', height: '174px' }" width="300">
-              <a-layout :style="{ background: 'transparent', padding: '0', height: '174px' }">
-                <a-layout-header :style="{ background: 'transparent', padding: '0', height: '48px' }">
+      <a-layout :style="{ background: 'transparent', padding: '0', height: '100%', padding: '0' }">
+        <a-layout-header :style="{ padding: '22px 30px 22px', color: 'white', height: '255px' }" class="print-header">
+          <a-layout :style="{ background: 'transparent', padding: '0' }">
+            <a-layout-sider :style="{ background: 'transparent', padding: '0' }" width="400">
+              <a-layout :style="{ background: 'transparent', padding: '0' }">
+                <a-layout-header :style="{ background: 'transparent', padding: '0', height: '32px' }">
                   <div class="community-header">
                     <div class="community-name">{{ houseSelect.communityName }}</div>
-                    <div class="community-price">
-                      <span>5月份参考均价</span>
-                      <span style="font-size:18px;font-weight: bold;text-align: center;">￥{{ houseSelect.averageLlistedPrice == null ? '--' : houseSelect.averageLlistedPrice.toLocaleString() }}元/m²</span>
-                    </div>
                   </div>
                 </a-layout-header>
                 <a-layout-content class="building-params">
@@ -48,33 +44,40 @@
                 </a-layout-content>
               </a-layout>
             </a-layout-sider>
-            <a-layout-content :style="{ background: 'transparent', padding: '0', height: '174px' }">
-              <a-layout :style="{ background: 'transparent', padding: '0 5px 0 34px', height: '174px', lineHeight: '1.4' }">
+            <a-layout-content :style="{ background: 'transparent', padding: '0' }">
+              <a-layout :style="{ background: 'transparent', padding: '0 5px 0 34px', lineHeight: '1.4' }">
                 <a-layout :style="{ background: 'transparent', padding: '0' }">
-                  <a-layout-sider :style="{ background: 'transparent', padding: '0'}" width="345">
+                  <a-layout-sider :style="{ background: 'transparent', padding: '0',position: 'relative'}" width="345">
                     <div class="price7label">近7日挂牌行情</div>
                     <div class="price7">87864元/㎡</div>
+                    <div class="price7-bg"></div>
                     <div class="price30label">近30日同比</div>
                     <div class="price30">
-                      <span>挂牌价持平</span>
-                      <span>挂牌量持平</span>
+                      <p>挂牌价 <span class="tag-less">减少</span></p>
+                      <p>挂牌量 <span class="tag-add">增加</span></p>
                     </div>
                   </a-layout-sider>
                   <a-layout-content :style="{ background: 'transparent', padding: '0', position: 'relative'}">
-                    <div style="position: absolute;bottom: 0px;font-size: 18px;line-height: 44px;margin-left: 92px;">未来60天成交价走势<span style="margin-left: 20px;display: inline-block;width: 89px;color: rgb(0,37,200);font-weight: bold;text-align: center;letter-spacing: 16px;">上涨</span></div>
+                    <div class="price60label">
+                      <span>未来60天成交价走势</span>
+                      <div class="price60-bg"></div>
+                      <span class="price60">上涨</span>
+                    </div>
                   </a-layout-content>
                 </a-layout>
-                <a-layout-footer :style="{ background: 'transparent', padding: '0', display: 'flex', height: '45px',lineHeight: '34px',paddingRight: '20px' }">
-                  <div class="sale-label">目前在售同户型房源 <span>1套</span></div>
-                  <div class="sent-buiding">参考租金</div>
-                  <div class="sent-label">房型<span>两室一厅</span></div>
-                  <div class="sent-label">价格<span>￥1000</span></div>
+                <a-layout-footer :style="{ background: 'transparent', padding: '0'}">
+                  <div class="print-sale">
+                    <div class="sent-label">目前在售同户型房源 <span>1套</span></div>
+                    <div class="sent-label">参考租金<span>4000元/月</span></div>
+                    <div class="sent-label">房型<span>两室一厅</span></div>
+                    <div class="sent-label">总售价<span>1000万</span></div>
+                  </div>
                 </a-layout-footer>
               </a-layout>
             </a-layout-content>
           </a-layout>
         </a-layout-header>
-        <a-layout-content :style="{ background: 'transparent', padding: '0'}">
+        <a-layout-content :style="{ background: 'transparent', padding: '0 24px'}">
           <div style="display:flex;height:100%">
             <div style="flex:1">
               <a-layout :style="{ background: 'transparent', padding: '0'}">
@@ -212,65 +215,117 @@ export default {
 <style scoped lang='less'>
 .building-overview {
   padding:0;
-  background: url(~@/assets/tmp/print-bg.png);
+  background: #FFFFFF;
   height: 792px;
   width: 1123px;;
 }
+.print-header{
+  background-image: url(~@/assets/print-bg.png) !important;
+  background-size: cover !important;
+}
 .community-header {
-  line-height: 48px;
+  line-height: 32px;
   display: flex;
 }
 .community-name {
   flex: 1;
-  font-size: 24px;
-}
-.community-price{
-  flex: 1;
-  line-height: 24px;
+  font-size: 30px;
+  font-weight: 600;
 }
 .price7label{
   font-size: 16px;
-  line-height: 35px;
+  line-height: 20px;
+  background: url(~@/assets/icons/icon_print_7.png) no-repeat;
+  background-position: 0 50%;
+  padding-left: 20px;
 }
-.price7{
-    line-height: 36px;
-    font-size: 20px;
-    color: rgb(0,37,200);
-    font-weight: bold;
-}
+.price7-bg{
+    width: 160px;
+    line-height: 32px;
+    border-width: 32px 32px 0px 0px;
+    border-style: solid  solid none none;
+    border-color: #fff transparent  transparent transparent;
+        margin: 10px 0 20px;
+  }
+  .price7{
+    font-weight: 600;
+    color: #030091;
+    font-size: 19px;
+    top: 30px;
+    position: absolute;
+    line-height: 32px;
+    left: 10px;
+  }
+  .price60label{
+  position: absolute;bottom: 0px;font-size: 16px;line-height: 24px;right: 0;
+  }
+  .price60-bg{
+      width: 75px;
+      line-height: 24px;
+      border-width: 0 0 24px 24px;
+      border-style: none none solid  solid;
+      border-color:  transparent  transparent #fff;
+      display: inline-block;
+      vertical-align: bottom;
+    }
+    .price60{
+      position: absolute;display: inline-block;width: 60px;color: rgb(0,37,200);font-weight: 600;text-align: center;font-size: 18px;right: 0;
+    }
 .price30label{
-  font-size: 16px;
-  line-height: 30px;
+    font-size: 16px;
+    line-height: 20px;
+    background: url(~@/assets/icons/icon_print_7.png) no-repeat;
+    background-position: 0 50%;
+    padding-left: 20px;
 }
 .price30{
     line-height: 20px;
-    font-size: 20px;
-    font-weight: bold;
+    font-size: 16px;
     display: flex;
-    span{
+    margin:10px 0 20px;
+    p{
       display: inline-block;
       flex: 1;
+      margin: 0;
+    }
+    span{
+      display: inline-block;
+      background-repeat: no-repeat;
+       background-position: 0 50%;
+       padding-left: 10px;
+       margin-left: 10px;
+      &.tag-less{
+        background-image: url(~@/assets/icons/icon_print_less.png);
+        color: #4BD2FD;
+      }
+      &.tag-add{
+        background-image: url(~@/assets/icons/icon_print_add.png);
+        color: #FF6A19;
+      }
+      &.tag-nochange{
+        background-image: url(~@/assets/icons/icon_print_add.png);
+        color: #9CEE45;
+      }
     }
 }
-.sale-label{
-  flex: 1;
+.print-sale{
+    width: 100%;
+    height: 37px;
+    display: flex;
+    line-height: 37px;
+    background: #fff;
+    border-radius: 3px;
+    margin-top: 20px;
+    padding: 0 5px;
 }
-.sent-buiding{
-  color: rgb(0,37,200);
-  font-size: 18px;
-  font-weight: bold;
-}
-.sent-label,.sale-label{
+.sent-label{
   color: rgb(0,37,200);
   font-size: 16px;
+  flex: auto;
   span{
-    font-size: 18px;
     font-weight: bold;
     margin-left: 12px;
   }
-}
-.sent-label {
-  margin-left: 24px;
 }
 .living-around{
   line-height: 1.4;
